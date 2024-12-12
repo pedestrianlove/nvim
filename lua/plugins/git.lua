bae79376ca0b0ef430ef6c482119089264793b40
J.S.LI <jsli@linux.com>
2024-12-12 23:13:46 +0800

    feat: add comment highlighting and autopairs, gitworktree

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -42,4 +42,27 @@ return {
 			vim.g.rhubarb_github_host = "github.com"
 		end,
 	},
+	{
+		"ThePrimeagen/git-worktree.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-telescope/telescope.nvim",
+		},
+		config = function()
+			require("git-worktree").setup()
+			require("telescope").load_extension("git_worktree")
+			vim.api.nvim_set_keymap(
+				"n",
+				"<leader>st",
+				":Telescope git_worktree<CR>",
+				{ desc = "[S]earch git_work[T]rees", noremap = true }
+			)
+			vim.api.nvim_set_keymap(
+				"n",
+				"<leader>cr",
+				":Telescope git_worktree create_git_worktree<CR>",
+				{ desc = "[C]reate git_wo[R]ktree", noremap = true }
+			)
+		end,
+	},
 }
